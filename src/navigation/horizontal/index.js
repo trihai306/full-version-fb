3f87f7775195eef6b817b3f5f34fa351688beb65
hai2k11 <trihai306201@gmail.com>
2023-03-12 22:37:37 +0700

    Initial commit

---
+++ src/navigation/horizontal/index.js
@@ -0,0 +1,7 @@
+// ** Navigation imports
+import apps from './apps'
+import pages from './pages'
+import uiElements from './ui-elements'
+
+// ** Merge & Export
+export default [...apps, ...uiElements, ...pages]
